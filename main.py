913303940f0d4a9723ae4c981d3117d23175e132
jadedragonette <jadedragonette@ymail.com>
2024-11-28 05:47:54 -0800

    Added circleshape to draw circles

--- main.py
+++ main.py
@@ -13,12 +13,16 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         pygame.Surface.fill(screen, color=000000, rect=None, special_flags=0)
+
+        dt = clock.tick(60) / 1000
 
     
 
